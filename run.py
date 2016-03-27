f3c34456ead26a603d5c69c456a0cade289c3e6f
matan157 <matan.uchen@gmail.com>
2016-03-27 18:04:48 +0000

    Add some comments hehe

--- run.py
+++ run.py
@@ -5,6 +5,7 @@ import sys
 import time
 import ConfigParser
 
+# Initialize ConfigParser
 config = ConfigParser.ConfigParser()
 config.read("config.ini")
 
@@ -13,10 +14,13 @@ CONSUMER_KEY = config.get("consumer", "key")
 CONSUMER_SECRET = config.get("consumer", "secret")
 ACCESS_KEY = config.get("access", "key")
 ACCESS_SECRET = config.get("access", "secret")
+
+# Set Authentication for Tweepy
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
 api = tweepy.API(auth)
 
+# Runes in the Background
 while True:
     api.update_status("Yes. #ShouldYouComment")
     time.sleep(3600)
